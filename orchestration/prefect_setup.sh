6288c4e318295cea0419745c1ce3843eacb8b97b
Promise Uzoagulu <uzoagulp@gmail.com>
2023-08-12 22:14:09 +0100

    added new files

---
+++ orchestration/prefect_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Setting up Prefect."
+
+echo "Create working queue."
+prefect work-queue create work_queue_1 || true
+
+echo "Creating storage. (interactive)"
+printf "1\n/tmp/store\n\nstoreName\ny\n" | prefect storage create
